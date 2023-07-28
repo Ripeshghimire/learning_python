d0ac86b72d0e22e74122026889838dea384f140e
Ripesh ghimire <ripeshghimire@Ripeshs-MacBook-Air.local>
2023-07-28 18:12:25 +0545

    practises added

--- nestedloops.py
+++ nestedloops.py
@@ -21,7 +21,7 @@
 #     for name1 in name:
 #         print(f'The name is  {name1}')
 
-#separate name with odd and even len in the word two different list 
+# separate name with odd and even len in the word two different list 
 # oddList = []
 # evenList = []
 # for outer in myArray:
@@ -84,25 +84,3 @@
 #         greater = per
 # print(f'THe greatest is {greater}')
 #     # print(f'The percentage of  {username} is {per}')
-
-# Write a function for checking the speed of drivers. This function should have one parameter: speed.
-# If speed is less than 70, it should print “Ok”.
-# Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit point 
-# and print the total number of demerit points. For example, if the speed is 80, it should print: “Points: 2”.
-# If the driver gets more than 12 points, the function should print: “License suspended”
-
-def checkSpeed(speed):
-    count_Point = 0
-    if speed < 70:
-        print("Ok ")
-    if speed > 70 :
-        speed = speed - 70 
-        remaining_speed  = speed // 5 
-        print(remaining_speed)
-        points = remaining_speed  
-        print(f'The number of points you have gained is {points}')
-    if points > 12 :
-        print("Liscence suspended")
-
-checkSpeed(900)
-
