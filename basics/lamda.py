652b61d6170d16d87bec8258c0f0253a83b77a04
Ripesh ghimire <ripeshghimire@Ripeshs-MacBook-Air.local>
2023-08-02 23:39:38 +0545

    changed

---
+++ basics/lamda.py
@@ -0,0 +1,22 @@
+#-----------------------------------Lambda Function--------------------------------------------
+# def sumFunction(num1,num2):
+#     return num1 + num2
+
+# result = sumFunction(11,12)
+# print(result)
+
+#-------------------------------------Syntax---------------------------------------------
+#Variable  = lambda argument:result 
+#----------------------------------Add two numbers----------------------------------------
+# result2 = lambda num1,num2:num1+num2
+# print(result2(2,3))
+#-----------------------------------Square a number--------------------------------------------
+# square = lambda num1 : num1 * num1
+# print(square(2))
+#----------------------------------Multiply two numbers-------------------------------------
+# mul = lambda num1,num2 : num1 * num2
+# print(mul(2,3))
+# --------------------------------Division of two numbers-----------------------------
+# div = lambda num1,num2:num1/num2
+# print(div(8,2))
+# 
